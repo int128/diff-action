c2d6241800df030fd0f60abd26cdda1c53fc8834
Hidetake Iwata <int128@gmail.com>
2021-08-12 09:32:35 +0900

    Remove eslint-plugin-github (#17)
    * yarn remove eslint-plugin-github
    
    * Use @typescript-eslint/recommended
    
    * yarn add -D @typescript-eslint/eslint-plugin
    
    * Fix lint error

--- src/comment.ts
+++ src/comment.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { GitHub } from '@actions/github/lib/utils'
 import { Diff } from './diff'
+import { GitHub } from '@actions/github/lib/utils'
 
 type Octokit = InstanceType<typeof GitHub>
 
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,15 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  try {
-    const outputs = await run({
-      base: core.getInput('base', { required: true }),
-      head: core.getInput('head', { required: true }),
-      label: core.getMultilineInput('label', { required: false }),
-      commentHeader: core.getInput('comment-header'),
-      commentFooter: core.getInput('comment-footer'),
-      token: core.getInput('token', { required: true }),
-    })
-    core.setOutput('different', outputs.different)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  const outputs = await run({
+    base: core.getInput('base', { required: true }),
+    head: core.getInput('head', { required: true }),
+    label: core.getMultilineInput('label', { required: false }),
+    commentHeader: core.getInput('comment-header'),
+    commentFooter: core.getInput('comment-footer'),
+    token: core.getInput('token', { required: true }),
+  })
+  core.setOutput('different', outputs.different)
 }
 
-main()
+main().catch((error) => core.setFailed(error))
