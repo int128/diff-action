7bde5bfbe3799ce15d031cb285986d47bf671afc
Hidetake Iwata <int128@gmail.com>
2024-04-21 17:57:23 +0900

    Migrate to ESM

--- src/format.ts
+++ src/format.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { Diff } from './diff'
+import { Diff } from './diff.js'
 
 type CommentOptions = {
   header: string
--- src/label.ts
+++ src/label.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { GitHubContext } from './github'
+import { GitHubContext } from './github.js'
 
 export const addLabels = async (github: GitHubContext, labels: string[]): Promise<void> => {
   if (labels.length < 1) {
