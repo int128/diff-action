c365124cb56c37808005b34b4cb84bde24896f8f
Hidetake Iwata <int128@gmail.com>
2025-11-29 19:32:42 +0900

    Migrate to @octokit/action

--- src/github.ts
+++ src/github.ts
@@ -1,24 +1,39 @@
-import * as github from '@actions/github'
-import * as pluginRetry from '@octokit/plugin-retry'
+import assert from 'node:assert'
+import * as fs from 'node:fs/promises'
+import { Octokit } from '@octokit/action'
+import { retry } from '@octokit/plugin-retry'
+import type { WebhookEvent } from '@octokit/webhooks-types'
 
-type Octokit = ReturnType<typeof github.getOctokit>
+export const getOctokit = () => new (Octokit.plugin(retry))()
 
-const getOctokit = (token: string): Octokit => github.getOctokit(token, undefined, pluginRetry.retry)
-
-export type GitHubContext = {
-  octokit: Octokit
-  owner: string
-  repo: string
+export type Context = {
+  repo: {
+    owner: string
+    repo: string
+  }
   eventName: string
-  issueNumber: number
-  workflowRunURL: string
+  runId: number
+  serverUrl: string
+  payload: WebhookEvent
 }
 
-export const getGitHubContext = (token: string): GitHubContext => ({
-  octokit: getOctokit(token),
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-  eventName: github.context.eventName,
-  issueNumber: github.context.issue.number,
-  workflowRunURL: `${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}`,
-})
+export const getContext = async (): Promise<Context> => {
+  // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+  return {
+    repo: getRepo(),
+    eventName: getEnv('GITHUB_EVENT_NAME'),
+    runId: Number.parseInt(getEnv('GITHUB_RUN_ID'), 10),
+    serverUrl: getEnv('GITHUB_SERVER_URL'),
+    payload: JSON.parse(await fs.readFile(getEnv('GITHUB_EVENT_PATH'), 'utf-8')) as WebhookEvent,
+  }
+}
+
+const getRepo = () => {
+  const [owner, repo] = getEnv('GITHUB_REPOSITORY').split('/')
+  return { owner, repo }
+}
+
+const getEnv = (name: string): string => {
+  assert(process.env[name], `${name} is required`)
+  return process.env[name]
+}
--- src/label.ts
+++ src/label.ts
@@ -1,41 +1,44 @@
 import * as core from '@actions/core'
-import type { GitHubContext } from './github.js'
+import type { Octokit } from '@octokit/action'
+import type { Context } from './github.js'
 
-export const addLabels = async (github: GitHubContext, labels: string[]): Promise<void> => {
+export const addLabels = async (labels: string[], octokit: Octokit, context: Context): Promise<void> => {
   if (labels.length < 1) {
     return
   }
-  if (!github.issueNumber) {
-    core.info(`Ignored non pull request event: ${github.eventName}`)
+  const issueNumber = inferIssueNumber(context)
+  if (!issueNumber) {
+    core.info(`Ignored non pull request event: ${context.eventName}`)
     return
   }
 
-  core.info(`Adding the labels to #${github.issueNumber}: ${labels.join(', ')}`)
-  const { data: added } = await github.octokit.rest.issues.addLabels({
-    owner: github.owner,
-    repo: github.repo,
-    issue_number: github.issueNumber,
+  core.info(`Adding the labels to #${issueNumber}: ${labels.join(', ')}`)
+  const { data: added } = await octokit.rest.issues.addLabels({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issueNumber,
     labels,
   })
   core.info(`Added the labels: ${added.map((l) => l.name).join(', ')}`)
 }
 
-export const removeLabels = async (github: GitHubContext, labels: string[]): Promise<void> => {
+export const removeLabels = async (labels: string[], octokit: Octokit, context: Context): Promise<void> => {
   if (labels.length < 1) {
     return
   }
-  if (!github.issueNumber) {
-    core.info(`Ignored non pull request event: ${github.eventName}`)
+  const issueNumber = inferIssueNumber(context)
+  if (!issueNumber) {
+    core.info(`Ignored non pull request event: ${context.eventName}`)
     return
   }
 
   for (const label of labels) {
-    core.info(`Removing the label from #${github.issueNumber}: ${label}`)
+    core.info(`Removing the label from #${issueNumber}: ${label}`)
     try {
-      await github.octokit.rest.issues.removeLabel({
-        owner: github.owner,
-        repo: github.repo,
-        issue_number: github.issueNumber,
+      await octokit.rest.issues.removeLabel({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: issueNumber,
         name: label,
       })
       core.info(`Removed the label: ${label}`)
@@ -46,6 +49,15 @@ export const removeLabels = async (github: GitHubContext, labels: string[]): Pro
       }
       throw error
     }
+  }
+}
+
+const inferIssueNumber = (context: Context): number | undefined => {
+  if ('pull_request' in context.payload) {
+    return context.payload.pull_request.number
+  }
+  if ('issue' in context.payload) {
+    return context.payload.issue.number
   }
 }
 
