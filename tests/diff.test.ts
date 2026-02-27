359c6707e19bc9a86e1dbd5a0499715300c61256
Hidetake Iwata <int128@gmail.com>
2026-02-27 11:05:34 +0900

    Add test case for rename with similarity index 100% (#1293)
    * Add test case for rename
    
    * Fix

--- tests/diff.test.ts
+++ tests/diff.test.ts
@@ -1,15 +1,33 @@
-import { expect, test } from 'vitest'
+import { expect, it } from 'vitest'
 import { computeDiff } from '../src/diff.js'
 
-test('diff', async () => {
+it('generates the array of Diff', async () => {
   const diffs = await computeDiff(`${__dirname}/fixtures/base`, `${__dirname}/fixtures/head`)
-  expect(diffs.length).toBe(4)
-  expect(diffs[0].baseRelativePath).toBeUndefined()
-  expect(diffs[0].headRelativePath).toBe('bar.txt')
-  expect(diffs[1].baseRelativePath).toBe('foo.txt')
-  expect(diffs[1].headRelativePath).toBeUndefined()
-  expect(diffs[2].baseRelativePath).toBe('deployment.yaml')
-  expect(diffs[2].headRelativePath).toBe('rollout.yaml')
-  expect(diffs[3].baseRelativePath).toBe('service.yaml')
-  expect(diffs[3].headRelativePath).toBe('service.yaml')
+  expect(diffs).toEqual([
+    {
+      baseRelativePath: undefined,
+      headRelativePath: 'bar.txt',
+      patch: expect.stringContaining('/dev/null'),
+    },
+    {
+      baseRelativePath: 'foo.txt',
+      headRelativePath: undefined,
+      patch: expect.stringContaining('/dev/null'),
+    },
+    {
+      baseRelativePath: 'gateway.yaml',
+      headRelativePath: 'httproute.yaml',
+      patch: expect.stringContaining('similarity index 100%'),
+    },
+    {
+      baseRelativePath: 'deployment.yaml',
+      headRelativePath: 'rollout.yaml',
+      patch: expect.stringContaining('@@'),
+    },
+    {
+      baseRelativePath: 'service.yaml',
+      headRelativePath: 'service.yaml',
+      patch: expect.stringContaining('@@'),
+    },
+  ])
 })
